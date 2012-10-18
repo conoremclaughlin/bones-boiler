24665466cff29e38b017e5ea19a61ed9b25296cc
Conor McLaughlin <conoremclaughlin@gmail.com>
2012-10-17 22:05:01 -0700

    Push, push, push.

--- servers/Route.bones.js
+++ servers/Route.bones.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
 var express = require('express');
-var yaml = require('yaml');
+var yaml = require('yaml-front-matter');
 var env = process.env.NODE_ENV || 'development';
 
 // TODO: add the app object and some easy way to apply the default template
@@ -12,32 +12,19 @@ var env = process.env.NODE_ENV || 'development';
 // Override this to add
 servers.Route.prototype.initializeStaticDir = function(link, path) {
     // Add static handler if page marked as complete.
-    // TODO: switch to mirror for serving static assets but we have no reverse
-    // proxy....
+    // TODO: switch to mirror for serving static assets
+    // but we have no reverse proxy....
     this.get(link, express['static'](path, {
         maxAge : env === 'production' ? 3600 * 1000 : 0
     }));
-};
-
-// Apply the application boilerplate.
-servers.Route.prototype.boiler = function(path) {
-    var page = require(path);
-    var view = new Views.App();
-    view.template({
-        page : page
-    });
-    return function(req, res, next) {
-        res.send(view.render().html());
-    };
 };
 
 // TODO: create a command to initialize new paths while the server is running.
 // Override this for custom directory structures.
 servers.Route.prototype.initializeStaticPages = function(app) {
     // Grab everything before and after the YAML front matter.
-    var regex = /^(\s*---([\s\S]+)---\s*)/gi;
-
     app.directories.forEach(function(dir) {
+        console.log('dir: ', dir);
         // only can use sync version here at initialization (blocking).
         // read the directories with a default base path of _site (else
         // check options)
@@ -49,29 +36,16 @@ servers.Route.prototype.initializeStaticPages = function(app) {
         });
 
         _.each(fileList, function(file) {
-            var page = fs.readFileSync(file, 'utf8');
-            var match = regex.exec(page);
-
-            // TODO: remove yaml parsing and just regex for needed
-            // properties.
-            if (match && match.length > 0) {
-                var yamlAttributes = match[2].replace(/^\s+|\s+$/g, '');
-                var attributes = yaml.eval(yamlAttributes);
-                // Must specify standard Jekyll permalink for now.
-                if (attributes.permalink && attributes.render) {
-                    // add the jekyll permalink url as a path. check whether it
-                    // needs to be added as static or processed through the base
-                    // template.
-                    // no idea if this will work or not......
-                    this.get(attributes.permalink, this.boiler(page));
-                    // if return a page before it hits the static serving of the
-                    // root directory.
-                }
+            var fileName = path.basename(file);
+            var front = yaml.loadFront(file, path.basename(file));
+            // Must specify standard Jekyll permalink for now.
+            if (front.url) {
+                this.get(front.url, servers.Page.page(fileName), servers.Page.send);
+                // if return a page before it hits the static serving of the
+                // root directory.
             }
         });
-
     }, this);
-    // read whether the
 };
 
 // TODO: add a check to see if any conflicts between dynamic and static
@@ -82,6 +56,6 @@ servers.Route.augment({
     initialize : function(parent, app) {
         parent.call(this, app);
         this.initializeStaticPages(app);
-        this.initializeStaticDir('/', '/_site');
+        this.initializeStaticDir('/', 'templates/compiled/public');
     }
 });
